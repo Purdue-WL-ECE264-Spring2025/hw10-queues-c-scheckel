2bac59a34ff3d08d3e8d47846c26570ddb4728d9
c-scheckel <scheckel@purdue.edu>
2025-04-04 15:59:30 -0400

    dq

--- queue.c
+++ queue.c
@@ -6,6 +6,8 @@ void enqueue(struct queue *q, struct game_state state) {
   insert_at_tail(&q->data, (size_t) serialized);
 }
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) { 
+  size_t serialized = remove_from_head(&q->data); 
+  return deserialize(serialized);}
 
 int number_of_moves(struct game_state start) { return 0; }
