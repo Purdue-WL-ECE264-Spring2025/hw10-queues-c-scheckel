40063fa25ac7deb3ec23e059053b3e44ba9757b6
c-scheckel <scheckel@purdue.edu>
2025-04-04 15:54:22 -0400

    enqu

--- queue.c
+++ queue.c
@@ -1,7 +1,10 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+  uint64_t serialized = serialize(state);
+  insert_at_tail(&q->data, (size_t) serialized);
+}
 
 struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
 
